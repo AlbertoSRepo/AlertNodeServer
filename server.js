2d101c8d9fbc1d2c2c418078376c1fc8c0589311
AlbertoSRepo <albesca97@gmail.com>
2023-11-14 09:44:23 +0100

    Server code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('Un client si è connesso');
+    
+    setInterval(() => {
+        socket.emit('messaggio', 'Ciao dal server!');
+    }, 60000); // invia un messaggio ogni 60 secondi
+});
+
+server.listen(3000, () => {
+    console.log('Server in ascolto sulla porta 3000');
+});
